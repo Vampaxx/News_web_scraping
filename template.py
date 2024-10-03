62add3adc2333ab45d6fe229dad5dd7b2f55d547
Vampaxx <arjunappu1001@gmail.com>
2024-10-03 12:49:10 +0530

    template [folder name change]

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "Database_API"
+project_name = "News_web_scraping"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
